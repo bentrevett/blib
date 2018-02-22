18620365faf2a5b45976eb6ea1edfeed8cc67520
Ben Trevett <Ben.Trevett@arm.com>
2018-02-22 15:50:21 +0000

    added one-to-one dataset. similar to the tensordataset but handles the case where the input sequences are variable lengths and you only want to pad to the maximum length within a batch.
    and to do that you use the new PadCollate class. currently only have a function for two inputs one output, but will add support for others as needed.

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Based off (i.e. some code copied from):
 Everything based on the `trainer`, which is a rip-off of __fastai__'s `learner`.
 
 Simlest way to use is:
+
 `trainer = blib.train.Trainer((train_dataloader, val_dataloader, test_dataloader), model, optimizer, criterion)`
--- blib/util/data.py
+++ blib/util/data.py
@@ -1,5 +1,26 @@
 import torch.utils.data
 
+class OneOneDataset(torch.utils.data.Dataset):
+
+    def __init__(self, input1, input2, output1):
+        """
+        The standard PyTorch dataset objects only handle single-input-single-output data loading and they must all be the same length already. 
+        This handles variable length sequences which means you can use a custom pad_collat_fn to handle only padding to the maximum length of a sequence WITHIN a batch, instead of maximum length sequence across all examples.
+        """
+
+        assert len(input1) == len(output1)
+
+        self.input1 = input1
+        self.output1 = output1
+
+
+    def __len__(self):
+        return len(self.input1)
+
+    def __getitem__(self, idx):
+        #self.output[idx] needs to be wrapped in a list to handle single values
+        return torch.LongTensor(self.input1[idx]), torch.LongTensor([self.output1[idx]])
+
 class TwoOneDataset(torch.utils.data.Dataset):
 
     def __init__(self, input1, input2, output1):
@@ -65,3 +86,41 @@ class TwoTwoDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         #self.output[idx] needs to be wrapped in a list to handle single values
         return torch.LongTensor(self.input1[idx]), torch.LongTensor(self.input2[idx]), torch.LongTensor([self.output1[idx]]), torch.LongTensor([self.output2[idx]])
+
+class TwoOnePadCollate:
+    """
+    This automatically pads variable length sequences within a batch to the maximum length sequence within that match
+    Use by passing collat_fn=TwoOnePadCollate() to the torch.utils.data.DataLoader
+    
+    THIS ASSUMES YOUR PADDING INDEX IS ZERO, IF NOT THEN YOU'RE INCORRECTLY PADDING
+
+    This may be slow, need to test.
+    """
+
+    def __init__(self, dim=0):
+        self.dim=dim
+
+    def pad_collate(self, batch):
+        #find longest sequence
+        input1_max_len = max(list(map(lambda x: x[0].shape[self.dim], batch)))
+        input2_max_len = max(list(map(lambda x: x[1].shape[self.dim], batch)))
+        
+        #used to store the padded sequences before stacking
+        _input1 = []
+        _input2 = []
+
+        #pad each to the maximum length within the batch
+        for i, (input1, input2, output1) in enumerate(batch):
+            _input1.append(torch.cat((input1, torch.zeros([input1_max_len-input1.shape[self.dim]]).long()),dim=dim))
+            _input2.append(torch.cat((input2, torch.zeros([input2_max_len-input2.shape[self.dim]]).long()),dim=dim))
+        
+        #stack all
+        input1 = torch.stack(list(map(lambda x: x, _input1)), dim=0)
+        input2 = torch.stack(list(map(lambda x: x, _input2)), dim=0)
+        output1 = torch.stack(list(map(lambda x: x[2], batch)), dim=0)
+
+        #return the batch
+        return input1, input2, output1
+
+    def __call__(self, batch):
+        return self.pad_collate(batch)
