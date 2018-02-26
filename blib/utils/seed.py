13dcb4868273ef8276243194c435ff156517b461
bentrevett <bentrevett@gmail.com>
2018-02-26 14:47:41 +0000

    + comments and check to the random seeding

--- blib/utils/seed.py
+++ blib/utils/seed.py
@@ -1,7 +1,13 @@
 import torch
 
 def set_seed(seed):
+    """
+    Sets the random seed for CPU, GPU and multi-GPU.
+    Returns nothing
+    """
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
+
+    assert torch.initial_seed() == seed, 'Seeding failed, somehow'
